078d1508267d201ef491291e1c33842a1bfe829a
David Radcliffe <radcliffe.david@gmail.com>
2016-08-15 12:04:46 -0400

    fixes for asset pipeline

--- lib/jquery-raty-rails/engine.rb
+++ lib/jquery-raty-rails/engine.rb
@@ -1,7 +1,4 @@
 module JqueryRatyRails
   class Engine < ::Rails::Engine
-    initializer 'jquery-raty-rails.load-jquery-raty-rails' do |app|
-      app.middleware.use ::ActionDispatch::Static, "#{root}/vendor"
-    end
   end
 end
